734a87bf12f80296fad1e3013b2f2f844a8c28f7
gaidaasd00 <swiftgaida@list.ru>
2022-06-19 20:16:28 +0300

    1. Add logic 2. Add function

--- TrafficLight2/ViewController.swift
+++ TrafficLight2/ViewController.swift
@@ -5,15 +5,58 @@
 //  Created by Алексей Гайдуков on 19.06.2022.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrenLight {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+   
+    @IBOutlet var stackLable: UIStackView!
+    @IBOutlet var redLable: UIView!
+    @IBOutlet var yellowLable: UIView!
+    @IBOutlet var greenLable: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrenLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLable.alpha = lightOff
+        yellowLable.alpha = lightOff
+        greenLable.alpha = lightOff
+        
+        startButton.layer.cornerRadius = 10
+    }
+    
+    override func viewWillLayoutSubviews() {
+        stackLable.layer.cornerRadius = stackLable.frame.width / 2
     }
 
-
+    @IBAction func startButtonTapped() {
+        startButton.setTitle(redLable.isHidden ? "START" : "NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLable.alpha = lightOn
+            greenLable.alpha = lightOff
+            currentLight = .yellow
+        case .yellow:
+            redLable.alpha = lightOff
+            yellowLable.alpha = lightOn
+            currentLight = .green
+        case .green:
+            yellowLable.alpha = lightOff
+            greenLable.alpha = lightOn
+            currentLight = .red
+        }
+    }
+    
 }
 
